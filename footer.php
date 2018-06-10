9f3d98a97305713784342f8bd620ba735a289feb
Rezaul Islam <rezaulislam.mcc@gmail.com>
2018-06-10 23:20:51 +0600

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,34 @@
+	</div><!-- .main -->
+	
+	<footer>
+	
+		<div class="fatfooter">
+	
+			<?php if ( is_active_sidebar( 'first-footer-widget-area' ) ) { ?>
+				<div class="one-third left widget-area first">
+					<?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+				</div><!-- .first .widget-area -->
+			<?php } ?>
+		
+			<?php if ( is_active_sidebar( 'second-footer-widget-area' ) ) { ?>
+				<div class="one-third left widget-area second">
+					<?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
+				</div><!-- .first .widget-area -->
+			<?php } ?>
+		
+			<?php if ( is_active_sidebar( 'third-footer-widget-area' ) ) { ?>
+				<div class="one-third left widget-area third">
+					<?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
+				</div><!-- .first .widget-area -->
+			<?php } ?>
+	
+		</div>
+		<div class="copyright-text" align="middle">
+		<p>Blog template built for <a href="http://mcc.com.bd">MCC Ltd</a> by <a href="https://thereza.herokuapp.com">@Reza</a>.</p>
+      </div>
+	</footer>
+	
+	<?php wp_footer(); ?>
+
+</body>
+</html>
